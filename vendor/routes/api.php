933c51c5bf52152194f612348d2affcd7c8ec06c
Al bassam Amer <al.a@myworkforce.com>
2020-12-01 15:03:54 +0800

    Fix vendor not verified Remove middlware for checking if email is verified in getting the current vendor

--- vendor/routes/api.php
+++ vendor/routes/api.php
@@ -40,7 +40,7 @@ Route::group(['prefix' => 'v1/vendor'], function() {
 	Route::post('/login', [LoginController::class, 'login']);
 	Route::post('/register', [RegisterController::class, 'register']);
 
-	Route::get('/current', [VendorController::class, 'currentUser'])->middleware(['auth:sanctum', 'vendor_is_verified']);
+	Route::get('/current', [VendorController::class, 'currentUser'])->middleware(['auth:sanctum']);
 	Route::get('/tokens', [VendorController::class, 'tokens'])->middleware(['auth:sanctum', 'vendor_is_verified']);
 	Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
