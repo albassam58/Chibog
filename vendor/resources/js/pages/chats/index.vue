e797b09c077001e8c8ca7d3695d8924991be3d02
Al bassam Amer <al.a@myworkforce.com>
2020-12-21 22:26:56 +0800

    Add pagination in message customers

--- vendor/resources/js/pages/chats/index.vue
+++ vendor/resources/js/pages/chats/index.vue
@@ -2,14 +2,8 @@
 	<div>
 		<v-container>
 			<v-row>
-				<v-col cols="6" class="d-flex flex-row">
+				<v-col cols="12" class="d-flex flex-row">
 					<div class="text-h4 mb-4 primary--text">Messages</div>
-				</v-col>
-				<v-col cols="6" class="d-flex flex-row-reverse">
-					<v-btn color="default" @click="$router.back(-1)" text>
-						<v-icon>mdi-arrow-left</v-icon>
-						Back
-					</v-btn>
 				</v-col>
 			</v-row>
 			<chat ref="chat"></chat>
